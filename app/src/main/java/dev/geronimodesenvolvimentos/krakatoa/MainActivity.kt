04be705d9f6fd3d5fb0596833912dfc70d57d354
dongeronimo <luciano.geronimo.fnord@gmail.com>
2026-02-07 10:36:11 -0300

    1st commit, setting manifest

---
+++ app/src/main/java/dev/geronimodesenvolvimentos/krakatoa/MainActivity.kt
@@ -0,0 +1,34 @@
+package dev.geronimodesenvolvimentos.krakatoa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import dev.geronimodesenvolvimentos.krakatoa.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'krakatoa' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'krakatoa' library on application startup.
+        init {
+            System.loadLibrary("krakatoa")
+        }
+    }
+}
